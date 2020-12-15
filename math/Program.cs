849f260571fb4dbf729e297ba4d879a04bbaae17
lathishmasuvarna <lathishmasuvarna@gmail.com>
2020-12-16 10:22:36 +1300

    Add project files.

---
+++ math/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+namespace math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, sum,diff,mul,div;
+            Console.WriteLine("enter first number");
+            a = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            b = Convert.ToInt64(Console.ReadLine());
+            sum = a + b;
+            diff = a - b;
+            mul = a * b;
+            div = a / b;
+            Console.WriteLine("the sum is={0}",sum);
+            Console.WriteLine("the diff is={0}", diff);
+            Console.WriteLine("the mul is={0}", mul);
+            Console.WriteLine("the div is={0}", div);
+            Console.ReadKey();
+        }
+    }
+}
